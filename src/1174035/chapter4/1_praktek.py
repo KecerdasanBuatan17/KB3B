18e44fa8dfa438d834d1d69b0d6dfcf8a8c70e02
sspitzenreiter <luthfimnabil17@gmail.com>
2020-03-23 20:08:08 +0700

    1174035/Luthfi Muhammad Nabil

---
+++ src/1174035/chapter4/1_praktek.py
@@ -0,0 +1,15 @@
+# In[1]: Import library dari pandas
+import pandas as pan
+#Membaca file csv menggunakan pandas
+data_forest = pan.read_csv('1_praktek.csv')
+# In[2]: untuk melihat jumlah dari baris data yang telah di import
+print(len(data_forest))
+# In[3]: untuk melihat lima baris pertama data yang telah di import
+print(data_forest.head())
+# In[4]: untuk mengetahui banyak baris dan kolom dari data yang
+# telah di import.
+print(data_forest.shape)
+#%%
+data_450 = data_forest[:450]
+#%%
+data_50 = data_forest[450:]
