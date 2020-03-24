06c2feaee641355a1309cfa5203b616b9b967af9
sspitzenreiter <luthfimnabil17@gmail.com>
2020-03-24 10:05:14 +0700

    Init

---
+++ src/1174050/chapter4/2.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 23 21:35:59 2020
+
+@author: User
+"""
+
+data_train = data_forest[:450]# memasukkan data training sebanyak 450 baris
+
+data_tes = data_forest[:450]# memasukkan data tes sebanyak 50
